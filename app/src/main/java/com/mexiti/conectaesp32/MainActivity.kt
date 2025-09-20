6e3e5fe335fc8bfdce3952c01d243f64fa942ec8
faraday58 <jarodriguezvera@gmail.com>
2025-09-20 13:23:49 -0600

    Código de Inicio

---
+++ app/src/main/java/com/mexiti/conectaesp32/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.mexiti.conectaesp32
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mexiti.conectaesp32.ui.theme.ConectaESP32Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ConectaESP32Theme {
+             /*   Scaffold(modifier = Modifier.fillMaxSize()) {
+                    innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }*/
+            }
+        }
+    }
+}
+
